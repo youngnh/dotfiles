a1388e6820cf602b522d5bf9b767c089f1fa040e
Nate Young <youngnh@gmail.com>
2024-06-04 09:19:25 -0500

    nix-darwin flake

---
+++ nix/nix-darwin-config/flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs; [
+        cabal-install clojure direnv fd fzf ghc git jq leiningen mr neovim
+        pkg-config ripgrep rlwrap stack starship stow tmux tree wget zlib zulu8
+      ];
+
+      # Install the development headers if they are part of a package's output
+      # environment.extraOutputsToInstall = [ "dev" "doc" "static" ];
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Configure bash as interactive shell
+      programs.bash.enable = true;
+      # programs.bash.enableCompletion = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .
+    darwinConfigurations."m-f95m2630kv" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."m-f95m2630kv".pkgs;
+  };
+}
